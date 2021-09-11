60e033204cfb38a692b0de179855ca1b33950f65
rynosk <73184813+rynosk@users.noreply.github.com>
2021-09-11 20:56:17 +0900

    upload
    load直後には画像が表示されない、resizeイベント後は表示される、解決方法がわからない。

---
+++ main.js
@@ -0,0 +1,25 @@
+class Renderer {
+    constructor(canvas) {
+        this.ctx = canvas.getContext("2d");
+        this.sizes = {
+            "w": document.documentElement.clientWidth,
+            "h": document.documentElement.clientHeight,
+        }
+        canvas.width = this.sizes.w;
+        canvas.height = this.sizes.h;
+        this.img = new Image();
+        this.img.src = "img01.png";
+        this.rendering();
+    }
+    rendering() {
+        this.ctx.fillStyle = "green";
+        this.ctx.fillRect(0, 0, this.sizes.w, this.sizes.h);
+        this.ctx.drawImage(this.img, 0, 0);
+    }
+}
+
+const canvas = document.querySelector("canvas");
+new Renderer(canvas);
+window.addEventListener("resize", () => {
+    new Renderer(canvas);
+});
